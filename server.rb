eb8cacacc3235eead019563ab082c329145e9d19
Thomas Chen <chen.thomas.c@gmail.com>
2015-06-10 23:11:39 -0400

    Limit number of entries per story
    * Limit number of entries per story to 10
    * Separate unfinished from finished stories on index page (WIP)

--- server.rb
+++ server.rb
@@ -17,7 +17,25 @@ get "/" do
 end
 
 get "/index" do
+  num_of_entries_per_gif = db_connection { |conn| conn.exec("SELECT stories.id AS story_id, stories.gif_url AS gif_url,
+    (SELECT COUNT(*) FROM entries WHERE entries.story_id = stories.id) AS entries_count FROM stories") }.to_a
+    binding.pry
+
+  unfinished_stories = []
+  finished_stories = []
+
+  num_of_entries_per_gif[0].each do |entry|
+    if entry['entries_count'] < 10
+      unfinished_stories << entry['story_id']
+    else
+      finished_stories << entry['story_id']
+    end
+  end
+
+  # NEED TO FINISH FIGURING OUT HOW TO SEPARATE FINISHED AND UNFINISHED STORIES
+
   continues = db_connection { |conn| conn.exec("SELECT gif_url, id FROM stories") }.to_a.reverse
+
   erb :index, locals: { continues: continues}
 end
 
@@ -43,13 +61,29 @@ get "/story/:story_id" do
 end
 
 post "/story/:story_id" do
-  db_connection { |conn| conn.exec_params("INSERT into entries(story_id, entry) VALUES ($1, $2)", [params['id'], params['story_bit']]) }
-  redirect "/story/#{params['id']}/full"
+  num_of_entries = db_connection { |conn| conn.exec("SELECT * FROM entries WHERE story_id = #{params['id']}") }.to_a.size
+
+  if num_of_entries < 10
+    db_connection { |conn| conn.exec_params("INSERT into entries(story_id, entry) VALUES ($1, $2)", [params['id'], params['story_bit']]) }
+    redirect "/story/#{params['id']}/full"
+  else
+    redirect "/story/#{params['id']}/full" #Should redirect to Completed Stories Page
+  end
 end
 
 get "/story/:story_id/full" do
+  num_of_entries = db_connection { |conn| conn.exec("SELECT * FROM entries WHERE story_id = #{params['story_id']}") }.to_a.size
+
   info = db_connection { |conn| conn.exec("SELECT stories.gif_url, stories.id, entries.entry FROM stories
     JOIN entries on stories.id = entries.story_id
     WHERE entries.story_id = #{params['story_id']}") }
-  erb :full, locals: { info: info.to_a }
+
+  if num_of_entries >= 10
+    notify = "That's it! The story is complete!"
+    erb :full, locals: { info: info.to_a, notify: notify }
+  else
+    notify = "The story so far"
+    erb :full, locals: { info: info.to_a, notify: notify }
+  end
+
 end
